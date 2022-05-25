d26a21eb7f351426071d9f7cc11b41b02f6c7137
John Barreto <john.barreto@api-ux.com>
2022-05-25 12:11:53 -0500

    base project

--- pages/index.js
+++ pages/index.js
@@ -5,21 +5,44 @@ import Newsletter from "components/Newsletter";
 import Footer from "components/Footer";
 import Layout from "components/Layout";
 
-import { resources } from "api/data";
 
-const Home = () => {
+const Home = ({ resources }) => {
   return (
     <>
       <Layout>
         <ResourceHighLight resources={resources} />
         <Newsletter />
         <ResourceList resources={resources} />
-
         <Footer />
       </Layout>
     </>
   )
 }
+
+//is called at the build time, and it´s called only once
+// export async function getStaticProps() {
+//   const resData = await fetch("http://localhost:3000/api/resources")
+//   const data = await resData.json();
+//   return {
+//     props: {
+//       resources: data
+//     }
+//   }
+// }
+
+//Is called every time you will visit the page 
+// function is executed on the server
+export async function getServerSideProps() {
+
+  const resData = await fetch("http://localhost:3000/api/resources")
+  const data = await resData.json();
+  return {
+    props: {
+      resources: data
+    }
+  }
+}
+
 
 export default Home;
 
