99f006fbf3fb4614d562b67b61cc107f1e809ac0
John Barreto <john.barreto@api-ux.com>
2022-05-25 13:12:47 -0500

    add resource form

--- pages/resources/new.js
+++ pages/resources/new.js
@@ -3,7 +3,23 @@ const { default: Layout } = require("components/Layout")
 const ResourceCreate = () => {
     return (
         <Layout>
-            <div>I am resource</div>
+            <div className="container">
+                <div className="columns">
+                    <div className="column is-8 is-offset-2">
+                        <div className="resource-form">
+                            <h1 className="title">Add New Resource</h1>
+                            <form>
+                                <div className="field">
+                                    <label className="label">Name</label>
+                                    <div className="control">
+                                        <input className="input" type="text" placeholder="Text input" />
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Layout>
     )
 }
