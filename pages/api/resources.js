df1b37129052f08ec19fb1000af3c2adfcb6d55c
John Barreto <john.barreto@api-ux.com>
2022-05-26 15:07:30 -0500

    send a post request and attach data in the request

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -1,7 +1,16 @@
 
 
 export default async function (req, res) {
-    const dataRes = await fetch("http://localhost:3001/api/resources");
-    const data = await dataRes.json();
-    res.send(data);
+
+    if (req.method == "GET") {
+        const dataRes = await fetch("http://localhost:3001/api/resources");
+        const data = await dataRes.json();
+        return res.send(data);
+    }
+
+    if (req.method == "POST") {
+        return res.send("Response successful");
+    }
+
+
 }
