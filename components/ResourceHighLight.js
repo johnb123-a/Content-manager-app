d9d24999bd3e0f0e5c77bc4b5a8e71a563b69dba
John Barreto <john.barreto@api-ux.com>
2022-06-06 16:49:04 -0500

    add link to detail page and fix styles in recourcelist

--- components/ResourceHighLight.js
+++ components/ResourceHighLight.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 
 const ResourceHighLight = ({ resources }) => {
@@ -16,6 +17,11 @@ const ResourceHighLight = ({ resources }) => {
                                                     <h2 className="subtitle is-4">{resource.createdAt}</h2>
                                                     <h1 className="title">{resource.title}</h1>
                                                     <p>{resource.description}</p>
+                                                    <Link href={`resources/${resource.id}`}>
+                                                        <a className="button">
+                                                            Details
+                                                        </a>
+                                                    </Link>
                                                 </div>
                                             </div>
                                         </div>
--- components/ResourceList.js
+++ components/ResourceList.js
@@ -1,37 +1,41 @@
+import Link from "next/link";
+import moment from "moment";
 
 const ResourceList = ({ resources }) => {
 
-    const renderResource = () => {
+    const renderResources = () => {
         return resources.map(resource =>
             <div key={resource.id} className="column is-5 is-offset-1 ">
                 <div className="content is-medium">
-                    <h2 className="subtitle is-5 has-text-grey">{resource.createdAt}</h2>
+                    <h2 className="subtitle is-5 has-text-grey">
+                        {moment(resource.createdAt).format("LLL")}
+                    </h2>
                     <h1 className="title has-text-black is-3">{resource.title}</h1>
-                    <p className="has-text-dark">{resource.description}</p>
+                    <p className="has-text-dark mb-2">{resource.description}</p>
+                    <Link href={`/resources/${resource.id}`}>
+                        <a className="button is-light">
+                            Details
+                        </a>
+                    </Link>
                 </div>
             </div>
         )
     }
 
-
     return (
-        <>
-            <section className="hero ">
-                <div className="hero-body">
-                    <div className="container">
-                        <section className="section">
-                            <div className="columns is-variable is-8">
-                                {
-                                    renderResource()
-                                }
-                            </div>
-                        </section>
-                    </div>
+        <section className="hero ">
+            <div className="hero-body">
+                <div className="container">
+                    <section className="section">
+                        <div className="columns is-multiline is-variable is-8">
+                            {renderResources()}
+                        </div>
+                    </section>
                 </div>
-            </section>
-
-        </>
+            </div>
+        </section>
     )
 }
+
 
 export default ResourceList
