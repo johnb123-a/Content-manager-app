7454078a4efaa3edb60a4bcebafdb5b0b2400e3d
John Barreto <john.barreto@api-ux.com>
2022-06-14 12:26:12 -0500

    add active resource component

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import ActiveResource from "./ActiveResource"
 import Navbar from "./Navbar"
 
 
@@ -5,6 +6,7 @@ const Layout = ({ children }) => {
     return (
         <>
             <Navbar />
+            <ActiveResource />
             {children}
         </>
     )
